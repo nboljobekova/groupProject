5e209045e9e2f2c5963662cda7474bebed4113db
aizadazh <46402847+aizadazh@users.noreply.github.com>
2019-04-15 22:48:01 +0600

    newheader for approval

--- dist/script/select.js
+++ dist/script/select.js
@@ -1,3 +1,31 @@
+var salons = {
+    filarr: [
+        {title: 'Amalie Beauty Studio', address: "ул. Рыскулова, 79Б", phone: "+996 555–13–05–05"},
+        {title: 'Profi Touch', address: "ул. Насирдина Исанова, 94", phone: "+996 550–99–97–67"},
+        {title: "Zarin Style", address: "Манаса проспект, 34", phone: "+996 709–87–11–60"},
+        {title: 'Маникюрный салон "Tomiris Nail Studio"', address: "ул. Киевская, 153", phone: "+996 554–814–884"},
+        {title: "Mystic Spa (Health & Beauty)", address: "Чуй проспект, 219", phone: "+996 555–45–44–33"},
+        {title: "FS Studio", address: "ул. Панфилова, 188/2", phone: "+996 555–74–23–30"},
+        {title: "Анталия 3", address: "Чуй проспект, 150", phone: "+996 777 283 110"},
+        {title: "La Letti", address: "пр. Чуй 150а перес. ул. Исанова, ТЦ Бета Сторес 1, этаж 2", phone: "+996 773 788 451"},
+        {title: "Istanbul", address: "ул. Киевская, 133", phone: "+996 700–41–41–46"},
+        {title: "Grace", address: "ул. Киевская, 135", phone: "+996 559–90–03–40"},        
+        {title: "Алтынай", address: "Манаса проспект, 47", phone: "+996 (312) 31–31–51"},
+        {title: "VERRA Beauty Bar", address: "ул. Токтогула, 161", phone: "+996 555–10–51–08"},       
+        {title: "Lash House Beauty", address: "ул. Абдымомунова, 259", phone: "+996 707 145 555"},
+        {title: "Jibsnails&Beauty", address: "Манаса проспект, 89", phone: "+996 778–22–23–19"},      
+        {title: "BigBro barber shop", address: "Чуй проспект, 178", phone: "+996 709–58–58–88"},          
+        {title: 'Керемет', address: "Чуй проспект, 170", phone: "+996 700–57–58–99"},
+    ]
+}
+
+var district = "filarr"
+function renderSalons(){
+    var selected_salons = salons[district]
+    /// Сформировать и добавить html на страницу
+}
+
+
 var filarr =
     [
         {title: 'Amalie Beauty Studio', address: "ул. Рыскулова, 79Б", phone: "+996 555–13–05–05"},
@@ -20,28 +48,26 @@ var filarr =
 var zum=(
 [
     {title: "Lisap Milano", address: "ул. Токтогула, 87", phone: "+996 554–28–02–79"}, 
-    {title: "GoldWell", address: "ул. Жумабек, 123", phone: "+996 555–05–35–55"}, 
-    {title: 'Beauty Showroom', address: "", phone: ""},
-    {title: 'Prestige', address: "", phone: ""},
+    {title: "GoldWell", address: "ул. Жумабек, 123", phone: "+996 555–05–35–55"},    
+    {title: 'Салон красоты "Prestige"', address: "ул. Киевская, 93", phone: "+996 (312) 62–31–31"},
     {title: 'Анталия', address: "ул. Касыма Тыныстанова, 189а", phone: "+996 500–97–66–36"}, 
-    {title: 'Beatrise', address: "", phone: ""},
-    {title: 'Akinay Beauty Maker', address: "", phone: ""},
+    {title: 'Салон красоты "Beatrise"', address: "Дом торговли, Юсупа Абдрахманова, 152", phone: "+996 550–63–03–32"},
+    {title: 'Студия красоты "Akinay Beauty Maker"', address: "ул. Панфилова, 153", phone: "+996 554–21–40–40"},
     {title: 'LAKSHMI Salon kg', address: "", phone: ""},
-    {title: 'Belladonna', address: "", phone: ""},
-    {title: 'Зухра', address: "", phone: ""},
+    {title: 'Салон красоты "BellaDonna"', address: "ул. Московская, 64", phone: "+996 555–37–26–47"},
+    {title: 'Салон красоты "Зухра"', address: "ул. Московская, 58", phone: "+996 700–10–81–84"},
     {title: 'Бейне', address: "", phone: ""},
-    {title: 'Diva Effect', address: "", phone: ""},
-    {title: 'Lolita', address: "", phone: ""},
+    {title: 'Салон красоты "Lolita"', address: "ул. Жоомарта Боконбаева, 76", phone: "+996 (312) 90–06–45"},
     {title: 'Beauty and Barber AVALON', address: "", phone: ""},
     {title: 'Алмаз', address: "", phone: ""},
     {title: 'Sofi Line', address: "", phone: ""},
     {title: 'Элиталь', address: "", phone: ""},
     {title: 'Мэрэлина', address: "", phone: ""},
     {title: 'Мариша', address: "", phone: ""},
-    {title:'Шарм', address: "", phone: ""},
-    {title:'Barbershef', address: "", phone: ""},
+    {title: 'Шарм', address: "", phone: ""},
+    {title: 'Barbershef', address: "", phone: ""},
     {title: 'Mon Ami', address: "", phone: ""},
-    {title:'Бонэ', address: "", phone: ""},
+    {title: 'Бонэ', address: "", phone: ""},
     {title: 'Life Style', address: "", phone: ""},
     {title: 'Sono Nails', address: "", phone: ""},
     {title: 'Imperatriz Beauty', address: "", phone: ""},
@@ -62,22 +88,26 @@ var zum=(
 var vost=(
 [
     {title: "BigBro barber shop", address: "Чуй проспект, 50", phone: "+996 709–58–88–58"}, 
-    {title: 'Samira', address: "", phone: ""},
+    {title: 'Samira', address: "Чуй проспект, 119", phone: "+996 559–39–39–54"},
     {title: 'Lashmaker.kg', address: "", phone: ""},
     {title: 'M_nails studio', address: "", phone: ""},
-    {title: 'Красотка', address: "", phone: ""},
-    {title: 'У Эрмека', address: "", phone: ""},
-    {title: 'Даяна', address: "", phone: ""},
-    {title: 'Alexandra Lash Studio', address: "", phone: ""},
-    {title: 'Aidina.lashes.ks', address: "", phone: ""},
+    {title: 'учебный центр "Красотка"', address: "Восток-5 микрорайон, 19", phone: "+996 555–45–13–07"},
+    {title: 'студия красоты "Красотка"', address: "Аламедин-1 микрорайон, 62а", phone: "+996 555–45–13–07"},
+    {title: 'Салон красоты "У Эрмека"', address: "Чуй проспект, 109а", phone: "+996 772–63–61–55"},
+    {title: 'Салон красоты "Даяна"', address: "Суюмбаева, 142", phone: "+996 700–99–19–98"},
+    {title: 'Студия наращивания ресниц "Alexandra Lash Studio"', address: "Восток-5 микрорайон, 6/3", phone: "+996 551–14–42–42"},
+    {title: 'Aidina.lashes.kg', address: "", phone: ""},
     {title: 'Карэ', address: "", phone: ""},
     {title: 'Стиль', address: "", phone: ""},
-    {title: 'Жизель', address: "", phone: ""},
+    {title: 'Парикмахерская "Жизель"', address: "Аламедин-1, Ауэзова, 7/1", phone: "+996 558–12–31–70"},
     {title: 'Best', address: "", phone: ""},
     {title: 'М Арт', address: "", phone: ""},
     {title: 'Адис', address: "", phone: ""},
     {title: 'Рахат М', address: "", phone: ""},
     {title: 'Ажар Style', address: "", phone: ""},
+    {title: 'Парикмахерская "Александра"', address: "Мусы Джалиля, 166", phone: "+996 708–15–10–94"},
+    {title: 'Салон красоты "Амелия"', address: "Аламедин-1 микрорайон, 51а", phone: "+996 500–99–69–60"},
+    {title: 'Парикмахерская "Сулуу"', address: "ул. Мусы Джалиля, 140", phone: "+996 709‒78‒11‒19"},
 ]),
 var yug =(
 [
@@ -114,6 +144,8 @@ var medakadem =
     {title: 'ОН И ОНА', address: "", phone: ""},
     {title: 'DIORA', address: "", phone: ""},
     {title: 'Viola', address: "", phone: ""},
+    {title: 'Салон красоты "Lash studio Karagat"', address: "Касыма Тыныстанова, 14", phone: "+996 505–50–31–66"},
+    
 ],
 var manas_gork =
 [
@@ -158,6 +190,7 @@ var mkrn3 =
     {title: 'Aleta', address: "", phone: ""},
     {title: 'Ak-kuu', address: "", phone: ""},
     {title: 'Ideal', address: "", phone: ""},
+    {title: 'Салон красоты "VIP"', address: "3-й микрорайон, 28", phone: "+996 559–77–72–57"},
 ],
 var mkrn4 =
 [
@@ -188,6 +221,7 @@ var mkrn7 =
     {title: 'La Letty студия эпиляции', address: "", phone: ""},
     {title: 'Happy', address: "", phone: ""},
     {title: 'Студия ногтевого сервиса "Nail Art Studio"', address: "", phone: ""},
+    {title: 'Студия наращивания ресниц "Kai lash studio"', address: "Суеркулова, 20/20", phone: "+996 700–45–54–44"},
 ],
 var mkrn8 =
 [
@@ -197,6 +231,7 @@ var mkrn8 =
     {title: 'Наташа', address: "", phone: ""},
     {title: 'АЗАЛИЯ', address: "", phone: ""},
     {title: 'Адель', address: "", phone: ""},
+    {title: 'Дом красоты "Жизель"', address: "ул. Токомбаева, 45/1", phone: "+996 552–999–555"},
 ],
 var mkrn9 =
 [
@@ -207,6 +242,7 @@ var mkrn9 =
 var mkrn10 =
 [
     {title: 'ДАРЬЯ', address: "", phone: ""},
+    {title: 'Cалон красоты "Лакшми"', address: "10-й микрорайон, 10", phone: "+996 555–71–07–21"},
 ],
 var mkrn11 =
 [
@@ -244,12 +280,15 @@ var osh =
     {title: 'K-studio', address: "", phone: ""},
     {title: 'ГУЛИЗА Style', address: "", phone: ""},
     {title: 'Амели', address: "", phone: ""},
-    {title: 'Каныкей', address: "", phone: ""},
+    {title: 'Парикмахерская "Каныкей"', address: "Ала-Тоо, 47а", phone: "+996 779–15–80–58"},
     {title: 'VIP', address: "", phone: ""},
+    {title: 'Парикмахерская "Сулуу"', address: "ул. Бейшеналиевой, 36/1 — 2 этаж", phone: "+996 708‒04‒59‒04"},
 ]
 
-for (var key in osh) {
-    out.innerHTML += key + "-" + osh[key] + '<br>';
+document.getElementById('district').onchange = function (displaySalonsOfDistrict)  {
+    //     var barbersSelected = var[0]
+    //     barbersSelect.options.length = 0;{
+
 }
 
 // document.getElementById('district').onchange = function(event) {
